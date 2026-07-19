8bfc2877df494142503bc3bd156238e4477763a2
Devarapalliprakash <devarapalliprakash06@gmail.com>
2026-07-19 12:36:50 +0530

    Delete Deepskilling/Module5 (EFCore) directory

--- Deepskilling/Module5 (EFCore)/src/Module5.Data/Entities/Genre.cs
+++
@@ -1,9 +0,0 @@
-namespace Module5.Data.Entities;
-
-public class Genre
-{
-    public int GenreId { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public virtual ICollection<BookGenre> BookGenres { get; set; } = new List<BookGenre>();
-}
